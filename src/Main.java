beafca599ddd9ddf0aedbb8c4264e4e9f834b4f6
BeanZip <buntonbrondon2010@gmail.com>
2024-09-16 15:47:34 -0400

    I need to code the file portion
    -Removed Size

--- src/Main.java
+++ src/Main.java
@@ -27,11 +27,12 @@ class Main{
     }
 
     static class Menu extends Window{
+        public int x;
+        public int y;
         JMenuBar menuBar = new JMenuBar();
         JMenu Help = new JMenu("Help");
         JMenu Options = new JMenu("Options");
 
-        JMenu adjust = new JMenu("Adjust");
         JMenu theme = new JMenu("Theme");
 
         JMenuItem tutorial = new JMenuItem("How This Works");
@@ -41,10 +42,6 @@ class Main{
         JMenuItem System = new JMenuItem("System Default");
         JMenuItem light = new JMenuItem("Light Mode");
 
-
-        JMenuItem large = new JMenuItem("Large");
-        JMenuItem medium = new JMenuItem("Default");
-        JMenuItem small = new JMenuItem("Small");
 
         public void interactive(){
             tutorial.addActionListener(e ->{
@@ -87,11 +84,7 @@ class Main{
             menuBar.add(Options);
             Help.add(tutorial);
             Help.add(GitHub);
-            Options.add(adjust);
             Options.add(theme);
-            adjust.add(large);
-            adjust.add(medium);
-            adjust.add(small);
             theme.add(Dark);
             theme.add(light);
             theme.add(System);
@@ -103,7 +96,9 @@ class Main{
     public static void main(String[] args){
         Menu m1 = new Menu();
 
-        m1.InitWindow(400,400);
+        m1.x = 600;
+        m1.y = m1.x/2;
+        m1.InitWindow(m1.x, m1.y);
         m1.menu();
         m1.interactive();
         m1.Visible(true);
