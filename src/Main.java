0940e33b95c210f22aa11d33371f8299f2d68a27
Ichi <78321273+nerdbro34@users.noreply.github.com>
2024-09-16 08:10:27 -0400

    Menu needs to be implemented

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+
+class Main{
+    static class Window {
+
+        JFrame frame = new JFrame("File Manager CL12");
+        boolean x = true;
+        public void InitWindow(int ScreenWidth,int ScreenHeight){
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(ScreenWidth,ScreenHeight);
+        }
+
+        public void Visible(){
+            frame.setVisible(x);
+            if(!frame.isVisible()){
+                System.out.print("Error Could not Initialize");
+            }
+            else {
+                JOptionPane.showMessageDialog(frame, "Window Successfully Deployed!");
+            }
+        }
+    }
+
+    static class Menu extends Window{
+        JMenuBar menuBar = new JMenuBar();
+        JMenu Help = new JMenu("Help");
+        JMenu Options = new JMenu("Options");
+
+        JMenu adjust = new JMenu("Adjust");
+        JMenu theme = new JMenu("Theme");
+
+        JMenuItem tutorial = new JMenuItem("How This Works");
+        JMenuItem GitHub = new JMenuItem("GitHub");
+
+        JMenuItem Dark = new JMenuItem("Dark Mode");
+        JMenuItem System = new JMenuItem("System Default");
+        JMenuItem light = new JMenuItem("Light Mode");
+    }
+}
